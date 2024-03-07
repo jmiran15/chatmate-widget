8e12fc3e47497634e05df2de3ffacec79b3b4efb
Jonathan Miranda <jmiran15@jhu.edu>
2024-03-06 20:14:40 -0500

    working with backend API at chatmate

---
+++ src/components/chat-container.jsx
@@ -0,0 +1,93 @@
+import React, { useState, useEffect } from "react";
+import ChatHistory from "./chat-history";
+import PromptInput from "./prompt-input";
+import handleChat from "@/utils/chat";
+import ChatService from "@/models/chatService";
+
+export default function ChatContainer({
+  sessionId,
+  settings,
+  knownHistory = [],
+}) {
+  const [message, setMessage] = useState("");
+  const [loadingResponse, setLoadingResponse] = useState(false);
+  const [chatHistory, setChatHistory] = useState(knownHistory);
+
+  // Resync history if the ref to known history changes
+  // eg: cleared.
+  useEffect(() => {
+    if (knownHistory.length !== chatHistory.length)
+      setChatHistory([...knownHistory]);
+  }, [knownHistory]);
+
+  const handleMessageChange = (event) => {
+    setMessage(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (!message || message === "") return false;
+
+    const prevChatHistory = [
+      ...chatHistory,
+      { content: message, role: "user" },
+      {
+        content: "",
+        role: "assistant",
+        pending: true,
+        userMessage: message,
+        animate: true,
+      },
+    ];
+
+    setChatHistory(prevChatHistory);
+    setMessage("");
+    setLoadingResponse(true);
+  };
+
+  useEffect(() => {
+    // this is where we call our api for a chat response
+    async function fetchReply() {
+      const promptMessage =
+        chatHistory.length > 0 ? chatHistory[chatHistory.length - 1] : null;
+      const remHistory = chatHistory.length > 0 ? chatHistory.slice(0, -1) : [];
+      var _chatHistory = [...remHistory];
+
+      if (!promptMessage || !promptMessage?.userMessage) {
+        setLoadingResponse(false);
+        return false;
+      }
+
+      console.log("fetchReply", promptMessage);
+
+      await ChatService.streamChat(
+        sessionId,
+        settings,
+        promptMessage.userMessage,
+        (chatResult) =>
+          handleChat(
+            chatResult,
+            setLoadingResponse,
+            setChatHistory,
+            remHistory,
+            _chatHistory
+          )
+      );
+      return;
+    }
+    loadingResponse === true && fetchReply();
+  }, [loadingResponse, chatHistory]);
+
+  return (
+    <div className="h-full w-full relative">
+      <ChatHistory settings={settings} history={chatHistory} />
+      <PromptInput
+        message={message}
+        submit={handleSubmit}
+        onChange={handleMessageChange}
+        inputDisabled={loadingResponse}
+        buttonDisabled={loadingResponse}
+      />
+    </div>
+  );
+}
