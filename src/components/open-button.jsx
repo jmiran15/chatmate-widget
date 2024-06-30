af39f264b0115743f33e7de93dec6030d9c05262
Jonathan Miranda <jmiran15@jhu.edu>
2024-06-29 21:46:18 -0400

    styling fix

--- src/components/open-button.jsx
+++ src/components/open-button.jsx
@@ -17,13 +17,9 @@ export default function OpenButton({ isOpen, toggleOpen, chatbot }) {
   const ChatIcon = CHAT_ICONS.hasOwnProperty(chatbot?.openIcon)
     ? CHAT_ICONS[chatbot.openIcon]
     : CHAT_ICONS.plus;
-
   const buttonClasses = `fixed bottom-[20px] right-[20px] z-[9999] flex items-center justify-center p-0 rounded-full bg-${
     colors[chatbot?.themeColor]
-  } text-white text-2xl border-none shadow-lg focus:outline-none cursor-pointer box-content button-hover-effect shadow-custom m-0 
-  text-white text-2xl max-w-[48px] w-[48px] max-h-[48px] h-[48px] border-none shadow-lg focus:outline-none cursor-pointer box-content button-hover-effect shadow-custom transition-custom
-  `;
-
+  } text-white text-2xl border-none shadow-lg focus:outline-none cursor-pointer box-content button-hover-effect shadow-custom m-0`;
   const iconClasses = "text-white w-[24px] h-[24px]";
 
   return (
